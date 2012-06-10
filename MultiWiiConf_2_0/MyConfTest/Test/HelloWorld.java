3b3ff9b4d419a040c2686a2ad2a5e2c25c436c27
Eric S <emanbehere@gmail.com>
2012-06-10 17:00:04 -0400

    Initial

---
+++ MultiWiiConf_2_0/MyConfTest/Test/HelloWorld.java
@@ -0,0 +1,6 @@
+//A Very Simple Example
+class HelloWorld {
+  public static void main(String[] args){
+    System.out.println("I'm a Simple Program");
+  }
+}
---
+++ MultiWiiConf_2_0/MyConfTest/Test/Make.bat
@@ -0,0 +1,2 @@
+javac  -classpath C:\Users\Eric\Desktop\MultiWii_2_0\MultiWiiConf_2_0\MyConfTest\Test HelloWorld.java
+pause
